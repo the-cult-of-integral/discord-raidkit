867b2f8910c315bac76e5d7b91b6fa292b6c1498
Catterall <kylejamescatterall16@gmail.com>
2020-08-06 06:34:03 +0100

    bug fix #1b

--- Thoth/main.py
+++ Thoth/main.py
@@ -24,7 +24,7 @@ def add_account():
         str_numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
         valid_token_chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
                              's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-                             '.']
+                             '.', '-']
         
         while not name:
             print(f"{Fore.LIGHTMAGENTA_EX}Please enter your target's discord name ({Fore.LIGHTRED_EX}do not include the "
