ae531d3a3135a29f3c2bcbc86b909bb45d11b791
Catterall <kylejamescatterall16@gmail.com>
2021-02-03 19:02:21 +0000

    Update v1.5.1b
    Updated to v1.5.1b; fixed search_for_update.

--- Thoth - Hitlist Program/main.py
+++ Thoth - Hitlist Program/main.py
@@ -21,7 +21,7 @@ def clear():
 
 
 def search_for_updates():
-    THIS_VERSION = "1.4.0"
+    THIS_VERSION = "1.5.1"
 
     header = {
         "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36",
